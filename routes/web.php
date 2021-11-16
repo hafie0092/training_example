f214ce7fa72a1dbe1a9556b1aae583d3ffedd920
hafie0092 <norhafizahyaakob@gmail.com>
2021-11-16 15:26:30 +0800

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/trainings', [App\Http\Controllers\TrainingController::class, 'index'])->name('training:index')->middleware('auth');
